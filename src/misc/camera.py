a7be1aee138c30fcb482cb99b23094a42bf8f72e
AlexandreKarakas <alexandre.karakas@gmail.com>
2020-03-25 12:45:58 +0100

    add code for camera

---
+++ src/misc/camera.py
@@ -0,0 +1,10 @@
+from picamera import picamera
+from time import sleep
+import classify_original
+
+camera = PiCamera()
+camera.start_preview()
+sleep(1)
+camera.capture('/home/pi/Desktop/image.jpg')
+camera.stop_preview()
+classify_original.analyse("/home/pi/Desktop/image.jpg")
