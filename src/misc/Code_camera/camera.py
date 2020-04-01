e0c71fdec7cad1c052ed473c06b74b74ab29bcc3
AlexandreKarakas <alexandre.karakas@gmail.com>
2020-04-01 12:10:32 +0200

    ajout fichier

--- src/misc/Code_camera/camera.py
+++ src/misc/Code_camera/camera.py
@@ -2,9 +2,10 @@ import picamera
 import time
 import classify_original
 
-camera = PiCamera()
-camera.start_preview()
-sleep(1)
-camera.capture('/home/pi/Desktop/image.jpg')
-camera.stop_preview()
-classify_original.analyse("/home/pi/Desktop/image.jpg")
+def captureImageAnalyse():
+    camera = PiCamera()
+    camera.start_preview()
+    sleep(1)
+    camera.capture('/home/pi/Desktop/image.jpg')
+    camera.stop_preview()
+    classify_original.analyse("/home/pi/Desktop/image.jpg")
