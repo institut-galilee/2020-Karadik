e31b2fb7fc893f5db27083f7833c68137549caec
AlexandreKarakas <alexandre.karakas@gmail.com>
2020-04-05 20:35:47 +0200

    correction bug

--- src/misc/Code_camera/camera.py
+++ src/misc/Code_camera/camera.py
@@ -5,7 +5,6 @@ import classify_original
 def captureImageAnalyse():
     camera = PiCamera()
     camera.start_preview()
-    sleep(1)
     camera.capture('/home/pi/Desktop/image.jpg')
     camera.stop_preview()
     classify_original.analyse("/home/pi/Desktop/image.jpg")
