b762899d96b0a671273d5a3109ea33f60e0cfdf9
AlexandreKarakas <alexandre.karakas@gmail.com>
2020-05-12 17:11:12 +0200

    ajout

--- src/misc/Code_camera/TestInterface.py
+++ src/misc/Code_camera/TestInterface.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 import camera
+import sonar as s
 
 class Interface (Frame) :
     def __init__(self, fenetre, **kwargs) :
@@ -14,10 +15,18 @@ class Interface (Frame) :
         self.bouton_quitter = Button(self, text="Quitter", command=self.quit)
         self.bouton_quitter.pack(side="left")
 
+        self.bouton_analyse = Button(self, text="Analyser la poubelle", command=self.analyse_contenu)
+        self.bouton_analyse.pack(side="middle")
+
         self.bouton_cliquer = Button(self, text="Prendre une photo", fg="blue", command=self.cliquer)
         self.bouton_cliquer.pack(side="right")
 
     def cliquer(self):
         value = camera.captureImageAnalyse()
         self.message = Label(self, text=value)
+        self.message.pack()
+
+    def analyse_contenu(self):
+        value = s.compute_occupancy_rate(1, 10)
+        self.message = Label(self, texte=value)
         self.message.pack()
